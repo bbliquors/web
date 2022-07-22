ab5bb85762fd30c64c0c929322838d9ceae1c421
bbliquors <77716352+bbliquors@users.noreply.github.com>
2022-07-21 20:05:36 -0700

    Delete Newsletter.tsx

--- components/Newsletter.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import news from '../newsletter/newsletter.png';
-import Image from 'next/image'
-import { NewsLetterContainer } from '../styles/muiStyles';
-
-export default function Newsletter() {
-
-  return (<NewsLetterContainer><Image alt="BBL News" src={news} layout="responsive" width={'400px'} height={'800px'} /></NewsLetterContainer>);
-}
